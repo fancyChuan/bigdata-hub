2f6977b88ef60c1cf85664e5224facd3645f9806
fancyChuan <1247375074@qq.com>
2019-04-01 09:33:28 +0800

    重构后测试mr

---
+++ hadoop/src/main/java/mapreduce/intro/Main.java
@@ -0,0 +1,26 @@
+package mapreduce.intro;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        testMaxTemperatureOnLocal();
+    }
+
+    /**
+     * 1. 没有给定资源文件的时候，默认使用本地的Hadoop，而本地的Hadoop并没有任何配置，是单机模式的，文件系统就是本地文件系统
+     */
+    public static void testMaxTemperatureOnLocal() throws Exception {
+        String input = "E:\\javaProject\\bigdata-learn\\hadoop\\input\\ncdc\\sample.txt";
+        String[] params = {input, "E:\\javaProject\\bigdata-learn\\hadoop\\out\\mr-max-temperature"};
+        MaxTemperature.testMaxTemperature(params);
+    }
+
+    /**
+     * 2. 给了虚拟机上的Hadoop资源文件，使用的是HDFS文件系统 TODO：还没执行成功
+     */
+    public static void testMaxTemperatureOnHadoop() throws Exception {
+        String input = "/user/beifeng/mapreduce/maxtemperature/input/sample.txt";
+        String[] params = {input, "/user/beifeng/mapreduce/maxtemperature/output"};
+        MaxTemperature.testMaxTemperature(params);
+    }
+}
