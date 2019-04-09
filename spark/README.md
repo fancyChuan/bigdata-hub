3b9dc1543b0c7ccb9e7814934eade7627457676d
fancyChuan <1247375074@qq.com>
2019-04-09 09:47:44 +0800

    Spark：RDD基础

--- spark/README.md
+++ spark/README.md
@@ -1,3 +1,22 @@
 ## Spark
 
 
+### 1. RDD基础
+RDD：对数据的抽象——弹性分布式数据集，每个RDD都被分为多个分区运行在不同节点上，可以包含python、java、scala中任意类型的对象，甚至是用户自定义对象。
+
+RDD.persist()可以让RDD缓存下来
+
+#### 1.1 创建RDD
+有两种方式：1. 读取外部数据集 2. 在驱动器程序中对一个集合进行并行化
+```
+# python
+sc.parallelize(["hello", "spark"])
+# java
+JavaRDD<String> lines = sc.parallelize(Arrays.asList("hello", "spark"))
+# scala
+val x = sc.parallelize(List("hello", "spark-shell"))
+``` 
+#### 1.2  RDD操作
+转化(transformation)操作：返回一个新的RDD，进行的是惰性求值，可以操作任意数量的输入RDD，比如rdd1.union(rdd2)
+
+行动(action)动作：触发实际计算，向驱动器程序返回结果或者把结果写入外部系统
--- spark/src/main/java/learningSpark/Main.java
+++ spark/src/main/java/learningSpark/Main.java
@@ -1,14 +1,45 @@
 package learningSpark;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        testWordCount();
+        // testWordCount();
+        // testHelloSpark();
+        testTransformation();
     }
 
     public static void testWordCount() {
         String input = "E:\\JavaWorkshop\\bigdata-learn\\spark\\src\\main\\resources\\testfile.md";
         String output = "E:\\JavaWorkshop\\bigdata-learn\\spark\\out\\wordcount";
         WordCount.main(new String[] {input, output});
+    }
+
+    /**
+     * 2. 实例化一个sc并将集合并行化
+     * 使用的是 JavaSparkContext 而不是 SparkContext
+     * python、scala使用的是SparkContext
+     */
+    public static void testHelloSpark() {
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("helloSpark");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        List<String> list = Arrays.asList("hello", "spark");
+        JavaRDD<String> lines = sc.parallelize(list);
+        System.out.println(lines);
+        System.out.println(lines.collect());
+    }
+
+    public static void testTransformation() {
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("helloSpark");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> inputRDD = sc.textFile("../resources/testfile.md");
+        inputRDD.filter(line -> {return line.contains("scala");});
     }
 }
