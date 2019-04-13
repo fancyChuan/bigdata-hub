c711b74e670527b1dc94a39f7e21fbe90c0807b2
fancyChuan <1247375074@qq.com>
2019-04-13 10:23:37 +0800

    Kafka：补充-重复消费

--- kafka/src/main/java/consumer/Consumer.java
+++ kafka/src/main/java/consumer/Consumer.java
@@ -1,10 +1,13 @@
 package consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 public class Consumer {
@@ -15,6 +18,8 @@ public class Consumer {
         props.put("bootstrap.servers", "s00:9092");
         // 制定consumer group
         props.put("group.id", "test");
+        // 重复消费的话，除了消费者组改名字，还要加下面一行
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // 重置offset，earliest表示从头开始消费，默认是最新值latest
         // 是否自动确认offset
         props.put("enable.auto.commit", "true");
         // 自动确认offset的时间间隔
@@ -28,9 +33,10 @@ public class Consumer {
 
         // 定义consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-
         // 消费者订阅的topic, 可同时订阅多个
         consumer.subscribe(Arrays.asList("first", "second", "third"));
+//        consumer.assign(Collections.singletonList(new TopicPartition("second", 0)));// 指定消费的主题和分区
+//        consumer.seek(new TopicPartition("second", 0), 2); // 指定从哪个地方开始消费
 
         while (true) {
             // 读取数据，读取超时时间为100ms
