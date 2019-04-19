4130a8306994c6ff3bd60acbe51186ab5b2bd327
touna-fancy <1247375074@qq.com>
2019-04-19 18:07:57 +0800

    Spark：创建Datasets

--- spark/SparkSQL.md
+++ spark/SparkSQL.md
@@ -6,4 +6,4 @@ spark用于处理结构化数据的一个模块，提供了除数据之外的额
 
 - 临时视图：df.createOrReplaceTempView("people") 是会话范围生效的，当前session失效视图也不可用
 - 全局视图：df.createGlobalTempView("people") 被绑定到 global_temp 类似于库，通过global_temp.people调用
-> 是整个application生效的，还是一直存在？？
+> 是整个application生效的，还是一直存在？？ 
--- spark/src/main/java/sparkSQL/Main.java
+++ spark/src/main/java/sparkSQL/Main.java
@@ -1,15 +1,17 @@
 package sparkSQL;
 
-import org.apache.spark.sql.AnalysisException;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import learningSpark.dataReadingAndSaving.Student;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.*;
 
-import java.util.HashSet;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.apache.spark.sql.functions.col;
 
 public class Main {
+    private static SparkSession spark = SparkSession.builder().master("local[1]").appName("hello-sparkSQL").getOrCreate();
 
     public static void helloSparkSQL() throws AnalysisException {
         SparkSession spark = SparkSession.builder().master("local[1]").appName("hello-word").getOrCreate();
@@ -31,7 +33,33 @@ public class Main {
         spark.sql("SELECT * FROM global_temp.student").show();
     }
 
+    public static void testCreateDataSet() {
+        // 从java bean创键
+        Student student = new Student("诸葛亮", 66.66, 66);
+        Encoder<Student> beanEncoder = Encoders.bean(Student.class);
+        Dataset<Student> dataset = spark.createDataset(Collections.singletonList(student), beanEncoder);
+        dataset.show();
+        // 从基本类型创建
+        Encoder<Integer> integerEncoder = Encoders.INT();// 创键一个int类型的encoder
+        Dataset<Integer> dataset1 = spark.createDataset(Arrays.asList(1, 2, 3, 4), integerEncoder);
+        // Dataset.map() 方法有两个重载方法，需要通过 (MapFunction<Integer, Integer>) 将Lambda表达式的返回值类型转换
+        Dataset<Integer> transformed = dataset1.map((MapFunction<Integer, Integer>) value -> value + 1, integerEncoder);
+        transformed.show();
+        System.out.println(transformed.collectAsList());
+        // DataFrame可以转为Dataset
+        String path = "E:\\JavaWorkshop\\bigdata-learn\\spark\\src\\main\\resources\\jsonFile.txt";
+        Dataset<Student> dataset2 = spark.read().json(path).as(beanEncoder);
+        dataset2.show();
+    }
+
+    public static void testJoin() {
+        Dataset<Row> dept = spark.read().json("E:\\JavaWorkshop\\bigdata-learn\\spark\\src\\main\\resources\\department.json");
+        Dataset<Row> emp = spark.read().json("E:\\JavaWorkshop\\bigdata-learn\\spark\\src\\main\\resources\\employee.json");
+        dept.join(emp, "id");
+    }
+
     public static void main(String[] args) throws AnalysisException {
-        helloSparkSQL();
+        // helloSparkSQL();
+        testCreateDataSet();
     }
 }
