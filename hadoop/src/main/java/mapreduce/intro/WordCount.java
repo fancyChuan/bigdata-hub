f6bcf41a9cc1c7fe0f57a7acb3756b35f84c04ea
fancyChuan <1247375074@qq.com>
2019-10-06 02:20:21 +0800

    Hadoop：MapReduce-wordcount

--- hadoop/src/main/java/mapreduce/intro/WordCount.java
+++ hadoop/src/main/java/mapreduce/intro/WordCount.java
@@ -23,16 +23,15 @@ public class WordCount {
         private final static  IntWritable mapOutputValue = new IntWritable(1);
 
         @Override
-        public void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
-                throws IOException, InterruptedException {
-            // line value
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            // 每一行的数据
             String lineValue = value.toString();
-
+            // 使用这个类来切分行数据的原因是效率很高。而splite使用的是正则表达式，数据量增大的时候效率会降低。但是JDK不推荐使用，不知道为啥
             StringTokenizer stringTokenizer = new StringTokenizer(lineValue);
-
             while(stringTokenizer.hasMoreTokens()) {
                 String wordValue = stringTokenizer.nextToken();
-                mapOutputKey.set(wordValue);
+                mapOutputKey.set(wordValue); // 也可以写成 new Text(wordValue)，但是一般不这么写，因为处理大数据量的时候会大量生成新的对象，容易导致GC从而影响性能
+                // context是MR的运行上下文，map阶段需要把数据传给reduce阶段
                 context.write(mapOutputKey, mapOutputValue);
             }
         }
@@ -44,15 +43,14 @@ public class WordCount {
         private   IntWritable outputValue = new IntWritable();
 
         @Override
-        protected void reduce(Text key, Iterable<IntWritable> values,
-                              Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable value: values) {
                 sum += value.get();
             }
             outputValue.set(sum);
             System.out.println(key + ": " + outputValue);
-            // context.write(key, outputValue);
+            context.write(key, outputValue); // 这里上下文把结果写入文件系统
         }
     }
 
@@ -69,10 +67,7 @@ public class WordCount {
         job.setJarByClass(this.getClass());
 
         // 3: set job :
-        // input -> map -> reduce -> output
-        Path inPath = new Path(args[0]);
-        FileInputFormat.addInputPath(job, inPath);
-
+        // 配置Mapper
         job.setMapperClass(WCMapper.class);
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
@@ -95,15 +90,18 @@ public class WordCount {
         // 增加reduce的个数
         // job.setNumReduceTasks(2);
 
+        // 配置输入输出流
+        Path inPath = new Path(args[0]);
+        FileInputFormat.addInputPath(job, inPath);
         Path outPath = new Path(args[1]);
         FileOutputFormat.setOutputPath(job, outPath);
-
+        // 提交job
         boolean isSucc = job.waitForCompletion(true);
         return isSucc ? 0 : 1;
     }
 
     public static void main(String[] args) throws Exception {
-        String[] params = {"hadoop/mapreduce/src/main/java/mr_intro/wordcount.txt", "out"};
+        String[] params = {"hadoop/src/main/java/mapreduce/intro/wordcount.txt", "hadoop/target/out"};
         int status = new WordCount().run(params);
         System.exit(status);
     }
