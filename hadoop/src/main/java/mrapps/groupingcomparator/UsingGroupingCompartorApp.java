030012db8a3167ce5dea29b7569d166e8cdc4fd9
fancyChuan <1247375074@qq.com>
2019-10-21 23:36:33 +0800

    Hadoop：MapReduce-Shuffle-Reduce的时候反序列化机制

--- hadoop/src/main/java/mrapps/groupingcomparator/UsingGroupingCompartorApp.java
+++ hadoop/src/main/java/mrapps/groupingcomparator/UsingGroupingCompartorApp.java
@@ -12,6 +12,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class UsingGroupingCompartorApp {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
@@ -20,7 +21,7 @@ public class UsingGroupingCompartorApp {
         }
         Job job = Job.getInstance(new Configuration());
 
-        job.setGroupingComparatorClass(OrderComparator.class);
+        job.setGroupingComparatorClass(OrderComparator.class); // 这个地方使用GroupingComparator
 
         job.setJarByClass(UsingGroupingCompartorApp.class);
         job.setMapperClass(OrderMapper.class);
@@ -54,7 +55,17 @@ public class UsingGroupingCompartorApp {
     static class OrderReducer extends Reducer<OrderBean, NullWritable, OrderBean, NullWritable> {
         @Override
         protected void reduce(OrderBean key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
-            context.write(key, NullWritable.get());
+            // context.write(key, NullWritable.get());
+
+            // TODO：需要好好捋一下为什么这个地方key其实也是多个。只不过因为key（也就是我们的OrderBean）的orderID相同
+            // 其实这个也是跟反序列化机制有关的，都是先新建key/value两个对象，然后从文件中读出数据反序列化之后装到key/value中
+            // 如果我们要拿到每个订单价格最高的前两个商品，那么代码如下：
+            Iterator<NullWritable> iterator = values.iterator();
+            for (int i = 0; i < 2; i++) {
+                if (iterator.hasNext()) {
+                    context.write(key, iterator.next());
+                }
+            }
         }
     }
 }
