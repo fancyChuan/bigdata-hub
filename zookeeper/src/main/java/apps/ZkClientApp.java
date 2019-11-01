6413f2dd0698ae4e0539974bc4b1464c07cc6d2a
fancyChuan <1247375074@qq.com>
2019-11-02 02:27:21 +0800

    ZooKeeper：监控服务器上下线示例

--- zookeeper/src/main/java/apps/ZkClientApp.java
+++ zookeeper/src/main/java/apps/ZkClientApp.java
@@ -1,9 +1,6 @@
 package apps;
 
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.ZooDefs;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
 import org.junit.Before;
 import org.junit.Test;
@@ -39,34 +36,64 @@ public class ZkClientApp {
 
     @Test
     public void create() throws KeeperException, InterruptedException { // 访问控制列表，可以控制那个IP那个节点可以访问
-        String result = zkCli.create("/forlearn", "from idea".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        String result = zkCli.create("/forlearn/test", "from idea".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         System.out.println(result);
     }
 
     @Test
     public void get() throws KeeperException, InterruptedException {
-        byte[] data = zkCli.getData("/forlearn", true, new Stat());
+        byte[] data = zkCli.getData("/forlearn/test", true, new Stat());
         String s = new String(data);
         System.out.println(s);
     }
 
     @Test
     public void set() throws KeeperException, InterruptedException {
-        Stat stat = zkCli.setData("/forlearn", "just do".getBytes(), 0); // 指定要修改的数据的版本
+        Stat stat = zkCli.setData("/forlearn/test", "just do".getBytes(), 0); // 指定要修改的数据的版本
         System.out.println(stat.getVersion());
         System.out.println(stat.getAversion());
     }
 
     @Test
     public void statAndDelete() throws KeeperException, InterruptedException {
-        Stat exists = zkCli.exists("/forlearn", false);
+        Stat exists = zkCli.exists("/forlearn/test", false);
         if (exists == null) {
             System.out.println("节点不存在");
         } else {
             System.out.println(exists.getDataLength());
-            zkCli.delete("/forlearn", exists.getVersion());
+            zkCli.delete("/forlearn/test", exists.getVersion());
             System.out.println("节点已经删除");
         }
     }
 
+    /**
+     * 实现一个循环注册的功能，也就是注册的节点发生变化的时候，重新注册
+     */
+    public void register() throws KeeperException, InterruptedException {
+        byte[] data = zkCli.getData("/forlearn", new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                try {
+                    register();
+                } catch (KeeperException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, null);
+        System.out.println(new String(data));
+    }
+
+    @Test
+    public void testRegister() {
+        try {
+            register();
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
