86fd69b95bafc21a844fe8ae6e394fe14001dc0a
fancyChuan <1247375074@qq.com>
2019-11-16 22:27:11 +0800

    Flink：概述、快速上手

---
+++ flink/src/main/scala/cn/fancychuan/scala/quickstart/DataSetWcApp.scala
@@ -0,0 +1,25 @@
+package cn.fancychuan.scala.quickstart
+
+import org.apache.flink.api.scala.{AggregateDataSet, DataSet, ExecutionEnvironment, _}
+
+/**
+  * Wordcount应用
+  *
+  * flink应用的4个步骤：
+  * 1. 创建环境env
+  * 2. 配置数据源source
+  * 3. 数据转换transform
+  * 4. 结果输出sink
+  */
+object DataSetWcApp {
+  def main(args: Array[String]): Unit = {
+
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+
+    val txtDataSet: DataSet[String] = env.readTextFile("hadoop/input/wordcount.txt")
+
+    val aggSet: AggregateDataSet[(String, Int)] = txtDataSet.flatMap(_.split(" ")).map((_, 1)).groupBy(0).sum(1)
+
+    aggSet.print()
+  }
+}
