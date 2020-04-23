28069c80b861d1e9024a9a7a40ab78f15a2d5117
hp-fancy <1247375074@qq.com>
2020-04-24 02:58:44 +0800

    Spark：修改代码组织方式

--- spark/src/main/java/learningSpark/rddProgram/Main.java
+++ spark/src/main/java/learningSpark/rddProgram/Main.java
@@ -1,21 +1,31 @@
 package learningSpark.rddProgram;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.Partition;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaDoubleRDD;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.junit.Test;
 import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 
 public class Main {
+    /**
+     * 实例化一个sc并将集合并行化
+     * 使用的是 JavaSparkContext 而不是 SparkContext
+     * python、scala使用的是SparkContext
+     */
+
     private static SparkConf conf = new SparkConf().setMaster("local").setAppName("javaSpark");
     private static JavaSparkContext sc = new JavaSparkContext(conf);
 
@@ -29,17 +39,18 @@ public class Main {
     }
 
     /**
-     * 2. 实例化一个sc并将集合并行化
-     * 使用的是 JavaSparkContext 而不是 SparkContext
-     * python、scala使用的是SparkContext
+     * 2. 创建RDD的两种方式
      */
-    public static void testHelloSpark() {
-//        SparkConf conf = new SparkConf().setMaster("local").setAppName("helloSpark");
-//        JavaSparkContext sc = new JavaSparkContext(conf);
+    @Test
+    public void testGenRdd() {
+        // 使用 sc.parallelize并行化
         List<String> list = Arrays.asList("hello", "spark");
         JavaRDD<String> lines = sc.parallelize(list);
         System.out.println(lines);
         System.out.println(lines.collect());
+        // 使用sc.textFile从文件中读取并创建为RDD
+        JavaRDD<String> inputRDD = sc.textFile("E:\\JavaWorkshop\\bigdata-learn\\spark\\src\\main\\resources\\testfile.md");
+        System.out.println(inputRDD.take(5));
     }
 
     /**
@@ -47,18 +58,22 @@ public class Main {
      */
     @Test
     public void testTransformation() {
-//        SparkConf conf = new SparkConf().setMaster("local").setAppName("helloSpark");
-//        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> inputRDD = sc.textFile("E:\\JavaWorkshop\\bigdata-learn\\spark\\src\\main\\resources\\testfile.md");
-        JavaRDD<String> scala = inputRDD.filter(line -> line.contains("Scala"));
-        JavaRDD<String> merge = scala.union(inputRDD.filter(line -> line.contains("Python")));
-        JavaRDD<Integer> lineWordcount = merge.map(line -> line.split(" ").length);
+//        JavaRDD<String> scala = inputRDD.filter(line -> line.contains("Scala"));
+//        JavaRDD<String> merge = scala.union(inputRDD.filter(line -> line.contains("Python")));
+//        JavaRDD<Integer> lineWordcount = merge.map(line -> line.split(" ").length);
+//        lineWordcount.mapPartitions(new FlatMapFunction<Iterator<Integer>, Object>() {
+//            @Override
+//            public Iterator<Object> call(Iterator<Integer> integerIterator) throws Exception {
+//
+//                return null;
+//            }
+//        });
 
         // 下面是行动函数
-        System.out.println(merge.count());
-        System.out.println(merge.collect());
-        System.out.println(merge.take(2));
-        System.out.println(merge.first());
+//        System.out.println(merge.count());
+//        System.out.println(merge.collect());
+//        System.out.println(merge.take(2));
+//        System.out.println(merge.first());
     }
 
     /**
@@ -192,16 +207,5 @@ public class Main {
         });
     }
 
-    public static void main(String[] args) {
-//        testWordCount();
-//        testHelloSpark();
-//        testTransformation();
-//        testPassFunction();
-//        testSample();
-//        testAction();
-//        testConvertAndMean();
-//        testPairRDD();
-//        testPartition();
-//        testDomainPartitioner();
-    }
+
 }
