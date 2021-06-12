50103cad32890db7e499361845c11299233cefc3
hp-fancy <1247375074@qq.com>
2021-06-12 22:43:30 +0800

    Flink：修改wordcount

--- flink/src/main/scala/cn/fancychuan/scala/quickstart/DataStreamWcApp.scala
+++ flink/src/main/scala/cn/fancychuan/scala/quickstart/DataStreamWcApp.scala
@@ -17,18 +17,19 @@ object DataStreamWcApp {
   def main(args: Array[String]): Unit = {
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
 
-    env.setParallelism(1) // 设置并行度为1
+    env.setParallelism(3) // 设置并行度为3
 
+    // 使用socket文本流
     val dataStream: DataStream[String] = env.socketTextStream("hadoop101", 7777)
 
     val sumdstream: DataStream[(String, Int)] = dataStream.flatMap(_.split(" "))
       .filter(_.nonEmpty)
       .map((_, 1))
       .keyBy(0) // 分组算子 0,1代表下标
-      .sum(1)
+      .sum(1) // 按第2个元素sum
 
     sumdstream.print()
 
-    env.execute()
+    env.execute("流处理wordcount作业")
   }
 }
