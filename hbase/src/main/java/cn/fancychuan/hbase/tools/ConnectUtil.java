fdaa2a8604ed5582647cbc53f21ae4095944d318
fancyChuan <1247375074@qq.com>
2021-08-11 23:49:21 +0800

    HBase：API-连接对象

--- hbase/src/main/java/cn/fancychuan/hbase/tools/ConnectUtil.java
+++ hbase/src/main/java/cn/fancychuan/hbase/tools/ConnectUtil.java
@@ -5,10 +5,24 @@ import org.apache.hadoop.hbase.client.ConnectionFactory;
 
 import java.io.IOException;
 
+/**
+ * 用于创建和关闭Connection对象
+ *
+ *  1. Configuration对象：
+ *      - 可以使用HBaseConfiguration.create()来创建
+ *      - 通过这种方式创建的Configuration对象既包含了hadoop的8个配置文件参数，
+ *        也包含了hbase-default.xml以及hbase-site.xml中的所有配置参数
+ */
 public class ConnectUtil {
 
     public static Connection getConnection() throws IOException {
         Connection connection = ConnectionFactory.createConnection();
         return connection;
+    }
+
+    public static void close(Connection connection) throws IOException {
+        if (connection != null) {
+            connection.close();
+        }
     }
 }
