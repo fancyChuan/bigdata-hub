1e728d600f002aef56fb033bde51fb10e1068d6e
hp-fancy <1247375074@qq.com>
2021-10-22 18:25:29 +0800

    ZooKeeper：监听机制补充

--- zookeeper/src/main/java/apps/ZkClientApp.java
+++ zookeeper/src/main/java/apps/ZkClientApp.java
@@ -15,12 +15,14 @@ public class ZkClientApp {
 
     private ZooKeeper zkCli;
     private static final String CONNECT_STRING = "hadoop101:2181,hadoop102:2181,hadoop103:2181";
+//    private static final String CONNECT_STRING = "dxbigdata101:2181";
     private static final int SESSION_TIMEOUT = 2000;
 
     /**
      * 初始化一个zk的客户端，并实例化一个监听器
      *  1. 监听器里面可以写用户所需业务逻辑，客户端收到事件通知后，就会调用监听器的process，也就是回调函数
-     *  2. 意味着，如果有多重业务逻辑，那么就需要有多个监听器，对应的需要多个客户端
+     *  2. 意味着，如果有多重业务逻辑，那么需要在process中判断监听的路径以及事项类型，从而嘉怡区分
+     *  TODO：new Zookeeper(string, timeout, watcher)中的watcher与在客户端zkCli.getData(string, watcher,null)中的watcher有啥不和？？
      */
     @Before
     public void init() throws IOException {
@@ -29,15 +31,21 @@ public class ZkClientApp {
                     @Override
                     public void process(WatchedEvent event) {
                         // 收到事件通知后的回调函数（用户的业务逻辑）
-                        System.out.println(event.getType() + " -- " + event.getPath());
-
-                        // 再次启动监听
-                        try {
-                            zkCli.getChildren("/", true);
-                        } catch (KeeperException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+                        System.out.println("客户端收到事件响应：" + event);
+                        String path = event.getPath();
+                        Event.EventType type = event.getType();
+                        // 使用if来判断收到的事件是哪个路径的，是什么类型的事件，并执行对应的逻辑
+                        if ("/forlearn/test".equals(path) && type == Event.EventType.NodeDataChanged) {
+                            System.out.println("====节点数据有变化，执行业务逻辑....");
+                            try {
+                                // 此处注册监听的目的，就是为了实现循环监听！
+                                zkCli.getData("/forlearn/test", true, null);
+                            } catch (KeeperException | InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        } else if (path.equals("/node_path") && type == Event.EventType.NodeChildrenChanged) {
+                            // NodeChildrenChanged 监听节点/node_path下的子节点的个数变化，不关心子节点的数据是否变化
+                            System.out.println("nononononono");
                         }
                     }
                 });
@@ -45,14 +53,14 @@ public class ZkClientApp {
 
     @Test
     public void ls() throws KeeperException, InterruptedException {
-        List<String> children = zkCli.getChildren("/", msg -> {
-            System.out.println("自定义的回调函数：" + msg);
+        List<String> children = zkCli.getChildren("/forlearn", msg -> {
+            System.out.println("ls函数中使用getChildren的自定义回调函数：" + msg);
         });
-        System.out.println("=============");
+        System.out.println("======= ls start ======");
         for (String child : children) {
             System.out.println(child);
         }
-        System.out.println("=============");
+        System.out.println("======= ls end ======");
         Thread.sleep(Long.MAX_VALUE); // 这里等待，为了跟服务端异步通信，能够接受服务端的通知
     }
 
@@ -73,10 +81,16 @@ public class ZkClientApp {
      */
     @Test
     public void get() throws KeeperException, InterruptedException {
-        byte[] data = zkCli.getData("/forlearn/test", true, new Stat());
+        byte[] data1 = zkCli.getData("/forlearn/test", true, new Stat());
+        byte[] data2 = zkCli.getData("/forlearn/test", new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                // 业务逻辑2
+                System.out.println("------getData 业务逻辑2");
+            }
+        }, null);
         zkCli.getChildren("/forlearn/test", true);
-        String s = new String(data);
-        System.out.println(s);
+        Thread.sleep(Long.MAX_VALUE);
     }
 
     @Test
