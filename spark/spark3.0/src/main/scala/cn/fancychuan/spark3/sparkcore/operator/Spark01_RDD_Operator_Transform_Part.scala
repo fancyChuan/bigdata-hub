f0e658249d5772a35eeea5f7940ae6e8a15893a8
hp-fancy <1247375074@qq.com>
2022-11-29 23:47:06 +0800

    Spark：RDD编程-转化算子

---
+++ spark/spark3.0/src/main/scala/cn/fancychuan/spark3/sparkcore/operator/Spark01_RDD_Operator_Transform_Part.scala
@@ -0,0 +1,24 @@
+package cn.fancychuan.spark3.sparkcore.operator
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark01_RDD_Operator_Transform_Part {
+
+    def main(args: Array[String]): Unit = {
+
+        val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+        val sc = new SparkContext(sparkConf)
+
+        // TODO 算子 - map
+        val rdd = sc.makeRDD(List(1,2,3,4),2)
+        // 【1，2】，【3，4】
+        rdd.saveAsTextFile("output")
+        val mapRDD = rdd.map(_*2)
+        // 【2，4】，【6，8】
+        mapRDD.saveAsTextFile("output1")
+
+        sc.stop()
+
+    }
+}
