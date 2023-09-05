5f57c4325bf449e4f80318ebe29da60b50f55ef9
hp-fancy <1247375074@qq.com>
2023-09-06 01:27:49 +0800

    Spark：RDD编程-转化算子sample/distinct

---
+++ spark/spark3.0/src/main/scala/cn/fancychuan/spark3/sparkcore/operator/Spark08_RDD_Operator_Transform_sample.scala
@@ -0,0 +1,38 @@
+package cn.fancychuan.spark3.sparkcore.operator
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark08_RDD_Operator_Transform_sample {
+
+    def main(args: Array[String]): Unit = {
+
+        val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+        val sc = new SparkContext(sparkConf)
+
+        // TODO 算子 - filter
+        val rdd = sc.makeRDD(List(1,2,3,4,5,6,7,8,9,10))
+
+        // sample算子需要传递三个参数
+        // 1. 第一个参数表示，抽取数据后是否将数据返回 true（放回），false（丢弃）
+        // 2. 第二个参数表示，
+        //         如果抽取不放回的场合：数据源中每条数据被抽取的概率，基准值的概念
+        //         如果抽取放回的场合：表示数据源中的每条数据被抽取的可能次数
+        // 3. 第三个参数表示，抽取数据时随机算法的种子
+        //                    如果不传递第三个参数，那么使用的是当前系统时间
+//        println(rdd.sample(
+//            false,
+//            0.4
+//            //1
+//        ).collect().mkString(","))
+
+        println(rdd.sample(
+            true,
+            2
+            //1
+        ).collect().mkString(","))
+
+
+        sc.stop()
+
+    }
+}
---
+++ spark/spark3.0/src/main/scala/cn/fancychuan/spark3/sparkcore/operator/Spark09_RDD_Operator_Transform_distinct.scala
@@ -0,0 +1,31 @@
+package cn.fancychuan.spark3.sparkcore.operator
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark09_RDD_Operator_Transform_distinct {
+
+    def main(args: Array[String]): Unit = {
+
+        val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+        val sc = new SparkContext(sparkConf)
+
+        // TODO 算子 - distinct 去重
+        val rdd = sc.makeRDD(List(1,2,3,4,1,2,3,4))
+
+        // map(x => (x, null)).reduceByKey((x, _) => x, numPartitions).map(_._1)
+
+        // (1, null),(2, null),(3, null),(4, null),(1, null),(2, null),(3, null),(4, null)
+        // (1, null)(1, null)(1, null)
+        // (null, null) => null
+        // (1, null) => 1
+        val rdd1: RDD[Int] = rdd.distinct()
+
+        rdd1.collect().foreach(println)
+
+
+
+        sc.stop()
+
+    }
+}
